785b72b6b277b8284df6afed2d5e25a7ee5b1554
Bruno Naibert <brunonaibert@hotmail.com>
2017-05-06 22:41:29 -0300

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,7 @@ var gulp = require('gulp'),
   clean = require('gulp-clean'),
   usemin = require('gulp-usemin'),
   htmlReplace = require('gulp-html-replace'),
-  browserSync = require('browser-sync').create(),
-  reload = browserSync.reload;
+  browserSync = require('browser-sync');
 
 // sass > css do tipo não minificado
 var sassBuild = {
@@ -21,47 +20,47 @@ var bases = {
 }
 
 var paths = {
-  scripts: ['src/js/**/*.js', '!src/js/libs/**/*.js'],
-  libs: ['src/js/libs/**/*.js'],
-  styles: ['src/css/**/*.css', '!src/css/libs/**/*.css'],
-  html: ['src/*.html'],
-  images: ['src/img/**/*'],
+  scripts: ['src/js/libs/**/*.js', 'src/js/**/*.js'],
+  styles: ['src/styles/**/*.scss'],
+  html: ['dist/*.html'],
+  images: ['dist/image/**/*'],
 }
 
-gulp.task('default', ['copy'], function() {
-  gulp.start('styles-build', 'scripts-build', 'build-html');
-});
-
 gulp.task('clean', function() {
+
   return gulp.src([bases.dist])
     .pipe(clean());
 });
 
 gulp.task('copy', ['clean'], function() {
+
   return gulp.src('src/img/**/*')
-    .pipe(gulp.dest('dist/img'))
+    .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('styles-build', function() {
+gulp.task('styles-build', ['copy'], function() {
+
   gulp.src(paths.styles)
     .pipe(sass(sassBuild)).on('error', sass.logError)
     .pipe(concat('style.min.css'))
-    .pipe(cssmin())
+    //.pipe(cssmin())
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('scripts-build', function() {
-  gulp.src(paths.libs, paths.scripts)
-    .pipe(concat('js.min.js'))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/js'));
-});
+//gulp.task('scripts-build', function() {
+  //gulp.src(paths.scripts)
+    //.pipe(concat('js.min.js'))
+    //.pipe(uglify())
+    //.pipe(gulp.dest('dist/js'));
+//});
 
-gulp.task('build-html', function() {
-  gulp.src('src/**/*.html')
-    .pipe(htmlReplace({
-      js: 'js/js.min.js',
-      css: 'css/style.min.css'
-    }))
-    .pipe(gulp.dest('dist'))
-});
+//gulp.task('build-html', function() {
+  //gulp.src('src/**/*.html')
+    //.pipe(htmlReplace({
+      //js: 'js/js.min.js',
+      //css: 'css/style.min.css'
+    //}))
+    //.pipe(gulp.dest('dist'))
+//});
+
+gulp.task('default', ['copy', 'styles-build'])
